ffd0f02d5530b23613b79a9ec24dec7b9d164493
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2024-11-07 13:06:22 -0300

    Removing interceptors

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -8,22 +8,4 @@ const api = axios.create({
   },
 });
 
-api.interceptors.request.use(
-  (config) => {
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
 export default api;
