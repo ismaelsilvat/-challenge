27ba0dac9cc1b03a53e744b93d50398db5c3a03b
ferreiratech <willferreiras1993@gmail.com>
2024-11-07 14:00:21 -0300

    create helper to migrate out of the component the responsibility for filtering the movies

--- src/features/character/components/CharacterCard.tsx
+++ src/features/character/components/CharacterCard.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import { Character } from "@/features/character/types/character";
 import Button from "@/components/Button";
+import { formatFilms } from '@/features/character/helpers/formatFilms';
 
 type CharacterCardProps = {
   character: Character;
@@ -8,10 +10,15 @@ type CharacterCardProps = {
 };
 
 const CharacterCard: React.FC<CharacterCardProps> = ({ character, onViewProfile }) => {
-  const featuredFilms = character?.films.slice(0, 3).join(', ') + (character?.films.length > 3 ? '...' : '');
+  const featuredFilms = formatFilms(character?.films);
   return (
     <div className="w-full md:w-[248px] md:min-h-[416px] flex flex-col items-center bg-background overflow-hidden">
-      <img src={character?.imageUrl} alt={character?.name} onClick={onViewProfile} className="w-full md:w-[248px] min-h-[248px] max-h-[248px] object-cover cursor-pointer" />
+      <img 
+        src={character?.imageUrl} 
+        alt={character?.name} 
+        onClick={onViewProfile} 
+        className="w-full md:w-[248px] min-h-[248px] max-h-[248px] object-cover cursor-pointer" 
+      />
       <div className="p-4 flex flex-col gap-2 justify-center text-center">
         <h3 className="font-lato text-lg font-bold text-defaultText">
           {character?.name}
@@ -20,7 +27,7 @@ const CharacterCard: React.FC<CharacterCardProps> = ({ character, onViewProfile 
           Featured Films
         </p>
         <p className="text-base leading-[16px] text-defaultText line-clamp-1">
-          {featuredFilms.length > 0 ? featuredFilms : "None"}
+          {featuredFilms}
         </p>
         <Button
           variant="text"
